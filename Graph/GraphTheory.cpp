8f91bf20309051d171b419ce4fcfb74eb04648c2
massoudsalem <massoudsalem@users.noreply.github.com>
2018-07-18 09:20:57 +0200

    Djikstra
    adding Djikstra with O(nlogn).

--- Graph/GraphTheory.cpp
+++ Graph/GraphTheory.cpp
@@ -5,6 +5,10 @@ bool vis[2000+5]; //visited array
 vector<int> AdjList[100000+5];
 vector<int> ToboSort;//topological sort vector
 int n,e,u,v; //n is nodes //e is edges //u is perent //v is child
+int shortestPath[100000+5];//shortest path array
+bool relaxed[100000+5];//relaxed array
+vector<pair<int,int>> djiAdjList[100000+5];//djikstra graph
+int shortest,minDis;//shortest child,minimum distance
 int CC=0;
 void CreatingAdjListGraph(){
 		cin>>n>>e;
@@ -14,9 +18,50 @@ void CreatingAdjListGraph(){
 			//AdjList[v].push_back(u); //undirected graph
 		}
 }
-void PreGraph(){	//Graph preparation
+void PreGraph(int n=0){	//Graph preparation
 	//memset(dis,-1,sizeof(dis));  //BFS distance array preparation
 	//memset(vis,0,sizeof(vis));	//DFS visited array preparation
+	// memset(relaxed, 0, sizeof(relaxed)); //djikstra relaxed array preparation
+	// fill(shortestPath,shortestPath+n, 1e9);	//djikstra shortestPath array preparation
+}
+void djikstra_k(int root){ //O(nlogn) cuz of priority_queue
+	priority_queue <pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	shortestPath[root]=0;
+	relaxed[root]=1;
+	for(auto v:djiAdjList[root]){
+		shortestPath[v.first]=v.second;
+		pq.push({v.second,v.first});
+	}
+	while(!pq.empty()){
+		minDis=pq.top().first,shortest=pq.top().second;
+		pq.pop();
+		if(!relaxed[shortest]){
+			relaxed[shortest]=1;
+			for(auto v:djiAdjList[shortest]){
+				if(shortestPath[v.first]>shortestPath[shortest]+v.second){
+					shortestPath[v.first]=v.second;
+					pq.push({v.second,v.first});
+				}
+			}
+		}
+	}
+}
+void dijkstra(int root){
+	priority_queue <pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	fill(shortestPath,shortestPath+n, 1e9);
+	shortestPath[root]=0;
+	pq.push({0,root});
+	while(!pq.empty()){
+		minDis=pq.top().first,shortest=pq.top().second;
+		pq.pop();
+		if(minDis>shortestPath[shortest])continue;
+		for(auto v:djiAdjList[shortest]){
+			if(shortestPath[v.first]>shortestPath[shortest]+v.second){
+				shortestPath[v.first]=shortestPath[shortest]+v.second;
+				pq.push({shortestPath[v.first],v.first});
+			}
+		}
+	}
 }
 void DFSRec(int root){	//Recursive
 		vis[root]=1;
